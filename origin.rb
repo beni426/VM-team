d4a045de486c3250acb565ff92ba4f77c81a1373
Ryu <goskn88006@yahoo.co.jp>
2022-03-13 17:15:42 +0900

    機能追加

--- origin.rb
+++ origin.rb
@@ -1,14 +1,14 @@
 require_relative '/home/beni/workspace/teamwork/VM-team/originorder.rb'
 class VendingMachine
-    attr_reader :order, :product, :changes, :user_selection, :money, :items
+    attr_accessor :order, :product, :changes, :user_selection, :money, :initialize_product
     MONEY = [10, 50, 100, 500, 1000].freeze
     def initialize
-        Order.initialize_product
-        
+        @order=Order.initialize_product
         @user_selection=''
         @slot_money = 0
-     
-    end
+        @total_sales = 0
+        
+     end
     def current_money
       @slot_money
     end
@@ -23,43 +23,65 @@ class VendingMachine
     end
     def get_change_required(current_money, product_cost)
       @change_required = current_money-product_cost
+      return current_money
+
     end
     def get_stock
-      Order.initialize_product.each do |product|
+       Order.initialize_product.each do |product|
         add_product(product)
       end
     end
     def add_product(product)
       puts "#{product[0]} の在庫の数を増やす"
       quatity = gets.chomp.to_i
-      Order.restock(product[0], quatity)
+      Order.initialize_product.restock(product[0], quatity)
     end
-    def add_money
-      slot_money(money)
+    def get_product_list
+      Order.initialize_product.each{|product|puts "在庫ありの商品：#{product[0]}" if product[2]>0} 
     end
-   def select_product
-     puts "ドリンクをお選びください"
-     Order.initialize_product.each{|product|puts "#{product[0]}:#{product[1]}"}
-      @user_selection= gets.chomp
-      return "おかねを追加します" unless current_money_enough?
-  end
-  def current_money_enough?
-    current_money >= Order.initialize_product[@user_selection.to_i][1]
-  end
- 
 
+   def select_product
+       get_product_list
+     puts "購入可能なドリンクをお選びください: 0: 'coke',1:'water',2: 'redbull'"
+     Order.initialize_product.each{|product|puts "#{product[0]}:#{product[1]} " if current_money >= product[1] }
+      @user_selection= gets.chomp
+      puts "お客様が"+ Order.initialize_product[@user_selection.to_i][0] +"を購入しました"
+      Order.initialize_product[@user_selection.to_i][2]-1
+         get_change
+         @slot_money= @slot_money-Order.initialize_product[@user_selection.to_i][1]
+         @total_sales += Order.initialize_product[@user_selection.to_i][1] 
+  end
+  # def choose(product)
+  #   @selection = product
+  #   raise 'ストックございません' unless Order.initialize_product[@user_selection.to_i][2] >0
+  #   Order.initialize_product[@user_selection.to_i][2]-1
+  #   return  Order.initialize_product[@user_selection.to_i][0]
+  # end
+
+  
   def get_change
     get_change_required(current_money,  Order.initialize_product[@user_selection.to_i][1])
   end
   def current_stock
-  
+    array=[Order.initialize_product[@user_selection.to_i][0],Order.initialize_product[@user_selection.to_i][2]-1]
+    puts array
+    # Order.initialize_product[@user_selection.to_i][2]=Order.initialize_product[@user_selection.to_i][2]-1 
   end
+  def total_sales
+    puts '現在の売上を表示'
+    @total_sales
+  end
+
 end
 
 
-# require_relative '/home/beni/workspace/teamwork/css/origin.rb'
+# require_relative '/home/beni/workspace/teamwork/VM-team/origin.rb'
 # vm=VendingMachine.new
 # vm.slot_money(100)
 # vm.current_money
+# vm.get_product_list
 # vm.select_product
-#vm.get_change
+# vm.get_change
+# vm.total_sales
+# vm.current_stock
+
--- originorder.rb
+++ originorder.rb
@@ -2,11 +2,10 @@ class Order
     
     attr_accessor :items , :selection, :product
     def self.initialize_product
-        
-        @items=[['coke',120,5],['water',100,5],['redbull',200,5]]
+         @items= [['coke',120,5],['water',100,5],['redbull',200,5]]
         end
     
-    def self.purchase(product)
+    def purchase(product)
         @selection = product
         raise 'ストックございません' unless @items[find(product)][2]>0
         @items[find(product)][2]-=1
@@ -15,7 +14,7 @@ class Order
     def self.restock(product, quatity)
         @items[find(product)][2]+=quatity
     end
-    def self.find(product)
+    def find(product)
         @items.find_index{|item|item[0] == product}
     end
 end
